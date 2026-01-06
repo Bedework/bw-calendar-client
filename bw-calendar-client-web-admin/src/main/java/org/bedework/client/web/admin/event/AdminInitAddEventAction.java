ec672b9156e188b92837c4ed52d308dd5c0455a3
mike <mikeadouglass@gmail.com>
2026-01-05 23:19:46 -0500

    update action to set current tab

--- bw-calendar-client-web-admin/src/main/java/org/bedework/client/web/admin/event/AdminInitAddEventAction.java
+++ bw-calendar-client-web-admin/src/main/java/org/bedework/client/web/admin/event/AdminInitAddEventAction.java
@@ -6,28 +6,37 @@ package org.bedework.client.web.admin.event;
 import org.bedework.client.web.admin.AdminUtil;
 import org.bedework.client.web.admin.BwAdminWebGlobals;
 import org.bedework.client.web.rw.event.InitAddEventAction;
-import org.bedework.webcommon.BwActionFormBase;
 import org.bedework.webcommon.BwRequest;
 import org.bedework.webcommon.BwSession;
+
+import org.apache.struts2.interceptor.parameter.StrutsParameter;
 
 /**
  * User: mike Date: 3/10/21 Time: 21:26
  */
 public class AdminInitAddEventAction extends InitAddEventAction {
+  private String currentTab;
+
   @Override
   public int doAction(final BwRequest request) {
-    final int fwd = super.doAction(request);
+    final var fwd = super.doAction(request);
+    final var sess = request.getSess();
+    final var globals = (BwAdminWebGlobals)request.getBwGlobals();
 
-    final BwSession sess = request.getSess();
-
+    globals.assignCurrentTab(currentTab);
     sess.embedContactCollection(request, BwSession.preferredEntity);
     sess.embedLocations(request, BwSession.preferredEntity);
 
-    if (((BwAdminWebGlobals)request.getBwGlobals()).getSuggestionEnabled()) {
+    if (globals.getSuggestionEnabled()) {
       AdminUtil.embedPreferredAdminGroups(request);
       AdminUtil.embedCalsuiteAdminGroups(request);
     }
 
     return fwd;
+  }
+
+  @StrutsParameter
+  public void setCurrentTab(final String val) {
+    currentTab = val;
   }
 }
